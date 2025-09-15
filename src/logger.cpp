4b23f2893d68467243e3d93af7ce4336b3294715
Sean Connolly <43280413+seataco@users.noreply.github.com>
2025-09-15 17:00:51 -0400

    Added basic function of each function - still failing pytest, file not found error. Also cleaned up executables and gitignore

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,6 +1,6 @@
-#include "logger.h"
 #include <string>
 #include <iostream>
+#include "logger.h"
 
 // ======================= TODOs =======================
 
@@ -8,27 +8,60 @@ void addReading(const std::string& label, double value,
                 std::string labels[], double values[],
                 int& size, int capacity) {
     // TODO: throw "Full" if size == capacity, else insert and ++size
+    if (size == capacity) {
+        throw std::string("Full");
+    }
+    labels[size] = label;
+    values[size] = value;
+    size++;
 }
 
 void updateValue(double* valuePtr, double newValue) {
     // TODO: write through pointer
+    *valuePtr = newValue;
 }
 
 void printReading(const std::string& label, const double& value) {
     // TODO: pretty-print one reading
-}
+    std::cout << label << ": " << value << std::endl;
+}   
 
 double average(const double values[], int size) {
     // TODO: throw "Empty" if size==0, else compute average
-    return 0.0;
+    if (size == 0) {
+        throw std::string("Empty");
+    }
+    double sum = 0.0;
+    for (int i = 0; i < size; i++) {
+        sum += values[i];
+    }
+    return sum / size;
 }
 
 double minValue(const double values[], int size) {
     // TODO: throw "Empty" if size==0, else compute min
-    return 0.0;
+    if (size == 0) {
+        throw std::string("Empty");
+    }
+    double min = values[0];
+    for (int i = 1; i < size; i++) {
+        if (values[i] < min) {
+            min = values[i];
+        }
+    }
+    return min;
 }
 
 double maxValue(const double values[], int size) {
     // TODO: throw "Empty" if size==0, else compute max
-    return 0.0;
+    if (size == 0) {
+        throw std::string("Empty");
+    }
+    double max = values[0];
+    for (int i = 1; i < size; i++) {
+        if (values[i] > max) {
+            max = values[i];
+        }
+    }
+    return max;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -62,5 +62,7 @@ int main(int argc, char* argv[]) {
     }
 
     // TODO: Free memory!
+    delete[] labels;
+    delete[] values;
     return 0;
 }
